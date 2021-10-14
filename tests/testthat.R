6429f3b9d6ff49461efcc91456b063245e75e247
millerh1 <millerh1@livemail.uthscsa.edu>
2021-10-14 17:26:43 -0500

    Cleaned tree ahead of bioconductor push

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(RLSeq)
-
-test_check("RLSeq")
--- tests/testthat/test-RLRangesFromRLBase.R
+++
@@ -1,3 +0,0 @@
-test_that(desc = "RLRangesFromRLBase works", {
-    expect_s4_class(RLRangesFromRLBase("SRX1070676"), "RLRanges")
-})
